55a5b3ab280f386be4de4c39f9dbf07f8abc0ad8
LCottin <cottin_leonard@icloud.com>
2024-06-12 17:37:59 +0200

    Made knife the default weapon

--- dev/common/Character/src/Character.cpp
+++ dev/common/Character/src/Character.cpp
@@ -85,7 +85,7 @@ void Character::_initCommon(const CharacterType type)
 
     _Sprite.setPosition(static_cast<Vector2f>(_Position));
 
-    _CurrentWeapon = make_unique<Pistol>("Pistol");
+    _CurrentWeapon = make_unique<Knife>("Knife");
     _CurrentWeapon->setPosition(_Position);
 
     _DamageTimer = seconds(1.0f);
